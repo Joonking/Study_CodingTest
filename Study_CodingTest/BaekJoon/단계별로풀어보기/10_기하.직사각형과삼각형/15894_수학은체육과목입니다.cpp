f8ba14c731025fbafae39b63f2eb326aacc11ecb
MinJoon Kim <kmj73203@naver.com>
2023-06-04 08:59:52 +0900

    6.04 수학

---
+++ Study_CodingTest/BaekJoon/단계별로풀어보기/10_기하.직사각형과삼각형/15894_수학은체육과목입니다.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	long long int n;
+	cin >> n;
+	cout << 4*n;
+}
+
+
+
+
