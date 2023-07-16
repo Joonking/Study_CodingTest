53e4a00b1456d85bf913012b18457d47313c07de
Minjoon Kim <kmj73203@naver.com>
2023-07-16 17:29:35 +0900

    7.16 test

--- Study_CodingTest/Test.cpp
+++ Study_CodingTest/Test.cpp
@@ -10,36 +10,10 @@
 using namespace std;
 
 
-std::string getString(std::string input_str) {
-    int n = input_str.length();
-    std::unordered_map<char, int> count;
-    std::string result;
-
-    // Count the occurrences of each character
-    for (int i = 0; i < n; i++) {
-        count[input_str[i]]++;
-    }
-
-    // Iterate through the characters in order
-    for (int i = 0; i < n; i++) {
-        char ch = input_str[i];
-
-        // Append the character to the result if it occurs more than once
-        if (count[ch] > 1) {
-            result.push_back(ch);
-            count[ch] = 0;  // Mark the occurrence as deleted
-        }
-    }
-
-    return result;
-}
-
 int main() {
-    std::string input_str;
-    std::cin >> input_str;
-
-    std::string result = getString(input_str);
-    std::cout << result << std::endl;
+    int a;
+    cin >> a;
+    cout << a;
 
     return 0;
 }
