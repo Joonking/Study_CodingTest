aba924e8eed8e894377d5ef77a99a5dfe0f45671
Joonking <kmj73203@naver.com>
2024-09-04 10:48:25 +0900

    9.04 문자열

---
+++ Study_CodingTest/BaekJoon/알고리즘분류/문자열/31403_A+B-C.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string A, B, C;
+	cin >> A >> B >> C;
+
+	cout << stoi(A) + stoi(B) - stoi(C) << endl;
+	cout << stoi(A + B) - stoi(C);
+}
