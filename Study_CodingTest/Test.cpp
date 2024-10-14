264e717cf71bf4910b1c79b1db736f661def23e4
Joonking <kmj73203@naver.com>
2024-10-14 15:05:42 +0900

    10.14 BFS 미로찾기

--- Study_CodingTest/Test.cpp
+++ Study_CodingTest/Test.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
-#include <queue>
-#include <stack>
 #include <vector>
-#include <string>
-
+#include <queue>
+#include <algorithm>
 using namespace std;
+
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-	
+    
 
-
-
-	return 0;
+    return 0;
 }
