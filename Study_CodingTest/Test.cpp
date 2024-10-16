1d7108b2f3076e4c7d61251436bf43e3184dfcdd
Joonking <kmj73203@naver.com>
2024-10-16 16:00:59 +0900

    10.16 그래프 2문제 BFS

--- Study_CodingTest/Test.cpp
+++ Study_CodingTest/Test.cpp
@@ -1,68 +1,94 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <queue>
+
 using namespace std;
 
-void DFS(int node);
+void BFS(const vector<vector<int>>& List, vector<int>& ColorList, int Num, bool* Find)
+{
+	queue<int> Queue;
+	Queue.push(Num);
+	ColorList[Num] = 1;
 
-static vector < vector <int> > A;
-static vector<int> check;
-static vector<bool> visited;
-static bool IsEven;
+	while (Queue.empty() == false)
+	{
+		int front = Queue.front();
+		Queue.pop();
+
+		for (int i : List[front])
+		{
+			if (ColorList[i] == 0)  //방문하지 않은 노드라면
+			{
+				//이전 노드랑 색상 다르게 칠하기 1이면 2, 2면 1
+				if (ColorList[front] == 1) 
+					ColorList[i] = 2;
+				else if (ColorList[front] == 2)
+					ColorList[i] = 1;
+
+				Queue.push(i);
+			}
+			else // 방문한 곳을 다시 만난 경우
+			{
+				//이전 노드와 색상이 같은 노드면 이분 그래프 안됨
+				if (ColorList[front] == ColorList[i])
+				{
+					*Find = true;
+					return;
+				}
+			}
+		}
+	}
+}
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
-    int N;
-    cin >> N;
+	int K;
+	cin >> K;
 
-    for (int t = 0; t < N; t++) {
-        int V, E;
-        cin >> V >> E;
-        A.resize(V + 1);
-        visited.resize(V + 1);
-        check.resize(V + 1);
-        IsEven = true;
-        for (int i = 0; i < E; i++) {
-            int S, E;
-            cin >> S >> E;
-            A[S].push_back(E);
-            A[E].push_back(S);
-        }
-        // 주어진 그래프가 하나로 연결되어 있다는 보장이 없으므로 모든 정점에서 수행
-        for (int i = 1; i <= V; i++) {
-            if (IsEven)
-                DFS(i);
-            else
-                break;
-        }
-        if (IsEven)
-            cout << "YES" << "\n";
-        else
-            cout << "NO" << "\n";
+	for (int i = 0; i < K; i++)
+	{
+		int V, E;
+		cin >> V >> E;
 
-        for (int i = 0; i <= V; i++) {
-            A[i].clear();
-            visited[i] = false;
-            check[i] = 0;
-        }
-    }
-}
+		//노드 리스트
+		vector<vector<int>> NodesList(V + 1);
+		//노드별 색상을 담을 리스트 
+		// !Point! : 인접 노드끼리 색상만 다르면 이분 그래프, 같으면 이분그래프 안됨 
+		//    0 : 방문하지 않은 노드
+		//    1 : 색상 1번
+		//    2 : 색상 2번
+		vector<int> Color(V + 1, 0);  
+		
+		//노드, 엣지 입력받기
+		for (int i = 0; i < E; i++)
+		{
+			int TempA, TempB;
+			cin >> TempA >> TempB;
+			NodesList[TempA].push_back(TempB);
+			NodesList[TempB].push_back(TempA);
+		}
+		bool Find = false;  //이분 그래프 아닌것을 찾자
 
+		//모든 노드가 다 이어지지 않은 경우 있을 수 있음.
+		//모든 노드에 대해서 BFS
+		for (int i = 1; i <= V; i++)  
+		{
+			if (Find)
+				break;  //이분 그래프 아니면 종료
+			if (Color[i] == 0)
+			{
+				BFS(NodesList, Color, i, &Find);
+			}
+		}
+		if (Find)
+			cout << "NO" << "\n";
+		else
+			cout << "YES" << "\n";
+	}
 
-void DFS(int node) { 	// DFS구현
-    visited[node] = true;
-    for (int i : A[node]) {
-        if (!visited[i]) {
-            check[i] = (check[node] + 1) % 2; 	// 인접한 정점은 같은 집합이 아니므로 이전 정점과 다른 집합으로 처리
-            DFS(i);
-        }
-        else if (check[node] == check[i]) { 	// 이미 방문한 정점이 현재 내 정점과 같은 집합이면 이분 그래프가 아님
-            IsEven = false;
-        }
-    }
+	return 0;
 }
