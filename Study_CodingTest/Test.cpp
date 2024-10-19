b5431f55ea1dba815af75dc5197f050dad71de24
Joonking <kmj73203@naver.com>
2024-10-19 21:34:16 +0900

    10.19 백준 해시를이용한집합과맵 듣보잡 수정

--- Study_CodingTest/Test.cpp
+++ Study_CodingTest/Test.cpp
@@ -1,35 +1,62 @@
 #include <iostream>
-#include <map>
-#include <unordered_map>
+#include <queue>
+#include <vector>
 
 using namespace std;
 
-int main() {
-    
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+void BFS(const vector<vector<int>>& List, vector<bool>& VisitedList, int Num)
+{
+	queue<int> Queue;
+	Queue.push(Num);
 
-    int N;
-    cin >> N;
+	while (Queue.empty() == false)
+	{
+		int Front = Queue.front();
+		Queue.pop();
+		
+		for (int i : List[Front])
+		{
+			if (VisitedList[i] == false)
+			{
+				VisitedList[i] = true;
+				Queue.push(i);
+			}
+		}
+	}
+}
 
-    unordered_map<int, int> UnOrderMap;
 
-    for (int i = 0; i < N; i++)
-    {
-        int temp;
-        cin >> temp;
-        UnOrderMap[temp]++;
-    }
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
-    int M;
-    cin >> M;
-    for (int i = 0; i < M; i++)
-    {
-        int temp;
-        cin >> temp;
-        cout << UnOrderMap[temp] << " ";
-    }
+	int N, M;
+	cin >> N >> M;
+	vector<vector<int>> NodeList(N+1);
 
-    return 0;
+	for (int i = 0; i < M; i++)
+	{
+		int TempA, TempB;
+		cin >> TempA >> TempB;
+		NodeList[TempA].push_back(TempB);
+		NodeList[TempB].push_back(TempA);
+	}
+
+	vector<bool> Visited(N + 1, false);
+
+	int Count = 0;
+	for (int i = 1; i <= N; i++)
+	{
+		if (Visited[i] == false)
+		{
+			BFS(NodeList, Visited, i);
+			Count++;
+		}
+	}
+
+	cout << Count;
+
+	return 0;
 }
