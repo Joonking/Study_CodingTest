208d5e32120a3b68c2acc23665a1f3d226830bc6
Joonking <kmj73203@naver.com>
2024-10-20 15:43:41 +0900

    10.20 Doit 책 분류 폴더 정리

--- Study_CodingTest/BaekJoon/Doit책분류/06_그래프/053_2252_줄세우기.cpp
+++ Study_CodingTest/BaekJoon/Doit책분류/06_그래프/053_2252_줄세우기.cpp
@@ -1,48 +1,48 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-
 using namespace std;
 
 int main()
 {
     ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-    //N : 학생 수, M : 비교 횟수
     int N, M;
     cin >> N >> M;
-    vector<vector<int>> A;
-    vector<int> indegree;
-    A.resize(N + 1);
-    indegree.resize(N + 1);
 
-    for(int i=0;i<M;i++)
+    vector<vector<int>> People(N + 1);
+    vector<int> Degree(N + 1, 0);
+
+    for (int i = 0; i < M; i++)
     {
-        int S, E;
-        cin >> S >> E;
-        A[S].push_back(E);
-        indegree[E]++;          // 진입차수 배열 데이터 저장하기
+        int A, B;
+        cin >> A >> B;
+        People[A].push_back(B);
+        Degree[B]++;
     }
 
-    queue<int> queue;  // 위상 정렬 수행
-    for (int i = 1; i <= N; i++) {
-        if (indegree[i] == 0) {
-            queue.push(i);
+    queue<int> Queue;
+    for (int i = 1; i <= N; i++)
+    {
+        if (Degree[i] == 0)
+            Queue.push(i);
+    }
+
+    while (Queue.empty() == false)
+    {
+        int Front = Queue.front();
+        Queue.pop();
+        cout << Front << " ";
+
+        for (int i : People[Front])
+        {
+            Degree[i]--;
+            if (Degree[i] == 0)
+                Queue.push(i);
         }
     }
 
-    while (!queue.empty()) {
-        int now = queue.front();
-        queue.pop();
-        cout << now << " ";
-        for (int next : A[now]) {
-            indegree[next]--;
-            if (indegree[next] == 0) {
-                queue.push(next);
-            }
-        }
-    }
-
+    return 0;
 }
