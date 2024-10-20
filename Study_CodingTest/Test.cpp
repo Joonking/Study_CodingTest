f205ac4e3261098744c0986844c21c5b9ec8a1ee
Joonking <kmj73203@naver.com>
2024-10-20 15:40:03 +0900

    10.20 위상정렬 폴더 추가

--- Study_CodingTest/Test.cpp
+++ Study_CodingTest/Test.cpp
@@ -1,28 +1,7 @@
 #include <iostream>
-#include <unordered_map>
+#include <vector>
+#include <queue>
 using namespace std;
-
-unordered_map<string, string> SNS;          // 친구 관계 부모 정보
-unordered_map<string, int> networkSize;     // 네트워크 크기 정보
-
-string FindFunction(string a)
-{
-    if (SNS[a] == a)
-        return a;
-    return SNS[a] = FindFunction(SNS[a]);  // 경로 압축
-}
-
-void UnionFunction(string a, string b)
-{
-    a = FindFunction(a);
-    b = FindFunction(b);
-
-    if (a != b)
-    {
-        SNS[b] = a;                      // b의 부모를 a로 갱신
-        networkSize[a] += networkSize[b]; // a에 b의 네트워크 크기 추가
-    }
-}
 
 int main()
 {
@@ -30,34 +9,38 @@ int main()
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    int T;
-    cin >> T;
-
-    for (int i = 0; i < T; i++)
+    int N, M;
+    cin >> N >> M;
+    
+    vector<vector<int>> People(N + 1);
+    vector<int> Degree(N + 1, 0);
+    
+    for (int i = 0; i < M; i++)
     {
-        int F;
-        cin >> F;
-        SNS.clear();
-        networkSize.clear();
+        int A, B;
+        cin >> A >> B;
+        People[A].push_back(B);
+        Degree[B]++;
+    }
 
-        for (int k = 0; k < F; k++)
+    queue<int> Queue;
+    for (int i = 1; i <= N; i++)
+    {
+        if (Degree[i] == 0)
+            Queue.push(i);
+    }
+
+    while (Queue.empty() == false)
+    {
+        int Front = Queue.front();
+        Queue.pop();
+        cout << Front << " ";
+
+        for (int i : People[Front])
         {
-            string A, B;
-            cin >> A >> B;
-
-            if (SNS.find(A) == SNS.end())
-            {
-                SNS[A] = A;           // A 자신을 부모로 설정
-                networkSize[A] = 1;   // 처음 시작할 때 A의 네트워크 크기는 1
-            }
-            if (SNS.find(B) == SNS.end())
-            {
-                SNS[B] = B;           // B 자신을 부모로 설정
-                networkSize[B] = 1;   // 처음 시작할 때 B의 네트워크 크기는 1
-            }
-
-            UnionFunction(A, B);
-            cout << networkSize[FindFunction(A)] << "\n";  // 합친 후 네트워크 크기를 출력
+            Degree[i]--;
+            if (Degree[i] == 0)
+                Queue.push(i);
         }
     }
 
