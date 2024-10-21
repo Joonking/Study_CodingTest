aec60d54218b1c80466587bc98c787cb26318233
Joonking <kmj73203@naver.com>
2024-10-21 17:59:31 +0900

    백준 다익스트라 2문제 1916 최소비용구하기, 1854 K번째최단경로찾기

--- Study_CodingTest/BaekJoon/Doit책분류/06_그래프/057_1916_최소비용구하기.cpp
+++ Study_CodingTest/BaekJoon/Doit책분류/06_그래프/057_1916_최소비용구하기.cpp
@@ -1,65 +1,61 @@
 #include <iostream>
-#include <queue>
 #include <vector>
-#include <limits.h>
+#include <climits>
+#include <queue>
+
 using namespace std;
 
 int main()
 {
     ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-    //N : 도시 개수, M : 버스 개수
     int N, M;
     cin >> N >> M;
 
-    typedef pair<int, int> node;
+    vector<vector<pair<int, int>>> DijkstraList(N + 1);
 
-    //그래프 인접 리스트
-    vector<vector<node>> CityList(N+1);
-    //비용 리스트
-    vector<int> CostList(N + 1, INT_MAX);
-    //방문 리스트
-    vector<bool> VisitList(N + 1, false);
 
-    for(int i=0;i<M;i++)
+    for (int i = 0; i < M; i++)
     {
-        int start, end, cost;
-        cin >> start >> end >> cost;
-        CityList[start].push_back({ cost, end});
+        int u, v, w;
+        cin >> u >> v >> w;
+        DijkstraList[u].push_back({ v, w });
     }
 
-    int StartCity, EndCity;
-    cin >> StartCity >> EndCity;
+    int Start, End;
+    cin >> Start >> End;
 
-    //알고리즘 구현 부분.
-    //우선순위 큐 선언
-	priority_queue<node, vector<node>, greater<node>> PQueue;
-    //첫번째 도시의 비용을 0으로 큐에 넣어줌.
-	PQueue.push({0, StartCity});
-    CostList[StartCity] = 0;
+    vector<int> Ans(N + 1, INT_MAX);
+    Ans[Start] = 0;
 
-    while(!PQueue.empty())
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> PQ;
+    PQ.push({ 0,Start });
+
+    while (PQ.empty() == false)
     {
-        node cur_node = PQueue.top();
-        PQueue.pop();
-        int now = cur_node.second;
-        //이미 방문한 도시가 아니라면
-        if (!VisitList[now])
+        int NowDistance = PQ.top().first;
+        int NowNode = PQ.top().second;
+        PQ.pop();
+
+        if (NowDistance > Ans[NowNode])
+            continue;
+
+        for (const auto& a : DijkstraList[NowNode])
         {
-            VisitList[now] = true;
-            for (node n : CityList[now])
+            int NextDistance = a.second;
+            int NextNode = a.first;
+
+            if (Ans[NextNode] > Ans[NowNode] + NextDistance)
             {
-                if (!VisitList[n.second] && CostList[n.second] > CostList[now] + n.first)
-                {
-                    CostList[n.second] = CostList[now] + n.first;
-                    PQueue.push({ CostList[n.second], n.second });
-                }
+                Ans[NextNode] = Ans[NowNode] + NextDistance;
+                PQ.push({ Ans[NextNode], NextNode });
             }
         }
     }
 
-    cout << CostList[EndCity];
-}
+    cout << Ans[End];
 
+    return 0;
+}
