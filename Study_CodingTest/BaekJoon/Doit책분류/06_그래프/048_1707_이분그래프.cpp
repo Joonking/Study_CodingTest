f2a3773632c6230957f0a77cc0837574df6afd1c
Joonking <kmj73203@naver.com>
2024-11-11 15:09:12 +0900

    [출첵 - 백준] DFS 14500 테르로미노

--- Study_CodingTest/BaekJoon/Doit책분류/06_그래프/048_1707_이분그래프.cpp
+++ Study_CodingTest/BaekJoon/Doit책분류/06_그래프/048_1707_이분그래프.cpp
@@ -13,7 +13,7 @@ static bool IsEven;
 
 int main()
 {
-    ios::sync_with_stdio(false);
+    ios::sYnc_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
--- Study_CodingTest/Test.cpp
+++ Study_CodingTest/Test.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-	ios::sync_with_stdio(false);
+	ios::sYnc_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
