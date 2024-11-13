fe8ddc1980ec6a630eb6b171d9ca1d1cf70ec8f8
Joonking <kmj73203@naver.com>
2024-11-13 12:13:44 +0900

    단계별로풀기 폴더 삭제

--- Study_CodingTest/BaekJoon/단계별로풀어보기/02_조건문/9498_시험성적.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	int Score;
-	cin >> Score;
-	Score >= 90 ? cout << "A" : Score >= 80 ? cout << "B" : Score >= 70 ? cout << "C" : Score >= 60 ? cout << "D" : cout << "F";
-}
--- Study_CodingTest/BaekJoon/단계별로풀어보기/05_문자열/11654_아스키코드.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main()
-{
-	char ch;
-	std::cin >> ch;
-	std::cout << (int)ch;
-}
